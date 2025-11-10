da7af1773e4ef38c25af7a6e6e12a4202c04f5c1
rudtn <H098S02308@chunchon.gwe.hs.kr>
2025-11-10 10:17:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,14 @@ import pandas as pd
 
 # --- 1. 페이지 기본 설정 ---
 st.set_page_config(
-    page_title="Streamlit 마법 교실",
+    page_title="Streamlit 지환의 첫번째 페이지",
     page_icon="🔮",
     layout="wide"
 )
 
 # --- 2. 페이지 타이틀 ---
 st.title("Streamlit 마법 교실 🔮")
-st.subheader("HTML/CSS를 활용해 멋진 효과를 만들어 봐요!")
+st.subheader("춘천고등학교")
 st.markdown("---") # 구분선
 
 # --- 3. 모든 커스텀 CSS ---
